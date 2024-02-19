39aa121e2b22ce313dcda6232104f1b65023073b
Brian Kariuki <kariukibriankamotho@gmail.com>
2024-02-19 14:45:16 +0300

    :)

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,6 +1,6 @@
 from django import forms
-from django.forms import modelForm
-from models import *
+from django.forms import ModelForm
+from .models import *
 
 class TaskForm(forms.ModelForm):
     class Meta:
