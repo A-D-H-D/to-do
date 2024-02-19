ee60d5a894f0e20438145c71c5c9f73e0092e365
Brian Kariuki <kariukibriankamotho@gmail.com>
2024-02-19 13:23:14 +0300

    initial commit

---
+++ tasks/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from django.forms import modelForm
+from models import *
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = '__all__'
